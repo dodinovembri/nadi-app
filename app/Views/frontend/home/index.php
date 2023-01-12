e43712048220a486c90a65719a80dc46f7fd4c3e
dodinovembri <dodinovembri32gmail.com>
2023-01-12 16:48:08 +0700

    update

--- app/Views/frontend/home/index.php
+++ app/Views/frontend/home/index.php
@@ -319,6 +319,40 @@
             </div>
         </section> <!--cat subscribe end-->
 
+        <!-- app two customer review start -->
+        <section class="brand-logo ptb-120">
+            <div class="container">
+                <div class="row justify-content-center">
+                    <div class="col-lg-7 col-md-10">
+                        <div class="section-heading text-center">
+                            <h2>Over 500+ Companies Trusted Us</h2>
+                            <p>Over 500+ Companies Trusted Us</p>
+                        </div>
+                    </div>
+                </div>
+                <div class="row">
+                    <ul class="brand-logo-grid list-unstyled">
+                        <li>
+                            <img src="<?= base_url('assets/img/brand-logo/1-2.png') ?>" alt="brand logo" />
+                        </li>
+                        <li>
+                            <img src="<?= base_url('assets/img/brand-logo/2-1.png') ?>" alt="brand logo" />
+                        </li>
+                        <li>
+                            <img src="<?= base_url('assets/img/brand-logo/3-1.png') ?>" alt="brand logo" />
+                        </li>
+                        <li>
+                            <img src="<?= base_url('assets/img/brand-logo/4-1.png') ?>" alt="brand logo" />
+                        </li>
+                        <li>
+                            <img src="<?= base_url('assets/img/brand-logo/5-1.png') ?>" alt="brand logo" />
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </section>
+        <!-- app two customer review end -->
+
         <!--footer section start-->
         <!--footer section start-->
         <footer class="footer-section">
