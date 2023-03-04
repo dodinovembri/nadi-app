c3b0899934dd5b233dc3370a42b135328898d5bc
dodinovembri <dodinovembri32@gmail.com>
2023-03-04 23:47:01 +0700

    update

--- app/Views/frontend/get_started/redirect.php
+++ app/Views/frontend/get_started/redirect.php
@@ -3,7 +3,7 @@
 
 <head>
     <meta charset="UTF-8">
-    <meta http-equiv="refresh" content="0; url=https://trial`.nadi-app.com/<?= $trial_domain ?>">
+    <meta http-equiv="refresh" content="0; url=https://trial.nadi-app.com/<?= $trial_domain ?>">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Redirection</title>
